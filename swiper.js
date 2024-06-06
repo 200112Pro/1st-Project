5208d8d9c370dfbcd5ea917f385f422189482a14
200112Pro <azagopromis@gmail.com>
2024-06-06 16:18:52 +0100

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,31 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 11,
+    loop:true,
+    grabCursor:true,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      breakpoints:{
+        0:{
+            slidesPerView: 1,
+            
+        },
+        520:{
+            slidesPerView: 2,
+            
+        },
+        950:{
+            slidesPerView: 3,
+            
+        },
+      }
+  });
+
